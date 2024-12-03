b9390d331dd073558395a242faf3133182254f00
Nursyuhada23 <147372985+Nursyuhada23@users.noreply.github.com>
2024-12-03 08:42:33 +0800

    Delete iteration 1 directory

--- iteration 1/generate_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This script hashes a given password
-$password = 'amirah123'; // Example password
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo $hashedPassword; // Output the hashed password
-?>
